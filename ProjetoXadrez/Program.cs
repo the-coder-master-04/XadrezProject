1f36cc0b9fcd6fd7ffe646716a57a2939846210f
Arthur Giovanelli <a.gioliveira05@gmail.com>
2025-03-26 17:48:53 -0300

    Adicionei a Classe Posição

--- ProjetoXadrez/Program.cs
+++ ProjetoXadrez/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Tabuleiro;
+
+
+Posicao P;
+
+P = new Posicao(3, 4);
+
+Console.WriteLine($"Posicão: {P}");
